a39470eb18e4c0c5aa36acebcf6cbbcd6b430d08
Peter Pavlovich <pavlovich@gmail.com>
2014-12-03 11:14:08 -0500

    Implement 'add task' functionality

--- client/main.js
+++ client/main.js
@@ -1,3 +1,17 @@
 UI.registerHelper('tasks', function(){
     return Tasks.find();
 });
+
+Template.body.events({
+    "submit .new-task": function (event) {
+        var text = event.target.text.value;
+        var newTask = {
+            name: text
+        };
+        Tasks.insert(newTask);
+
+        event.target.text.value = "";
+
+        return false;
+    }
+});
