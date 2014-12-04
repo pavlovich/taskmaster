32bc78c6fe7c13eda389da23f6376085e16b0c9a
Peter Pavlovich <pavlovich@gmail.com>
2014-12-04 04:14:07 -0500

    Add public/private attribute + ability to turn on and off

--- client/templates/tasks/item.js
+++ client/templates/tasks/item.js
@@ -2,5 +2,14 @@
 Template.taskItem.events({
     "click .delete-button": function () {
         Meteor.call('deleteTask', this._id);
+    },
+    "click .toggle-private": function () {
+        Meteor.call("setPrivate", this._id, !this.private);
+    }
+});
+
+Template.taskItem.helpers({
+    isOwner: function () {
+        return this.owner == null || this.owner === Meteor.userId();
     }
 });
