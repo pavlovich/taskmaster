29f2f18c428adeb84f0e71cfd6ba1b1966819bc9
Peter Pavlovich <pavlovich@gmail.com>
2014-12-03 22:51:20 -0500

    Add task creator's email to DB and display in list

--- client/main.js
+++ client/main.js
@@ -6,12 +6,25 @@ Template.body.events({
     "submit .new-task": function (event) {
         var text = event.target.text.value;
         var newTask = {
-            name: text
+            name: text,
+            email: Meteor.userEmail()
         };
         Tasks.insert(newTask);
 
         event.target.text.value = "";
 
         return false;
+    }
+});
+
+Meteor.startup(function () {
+    Meteor.userEmail = function () {
+        var currentEmail = null;
+        try {
+            currentEmail = Meteor.user().emails[0].address;
+        } catch (e) {
+            currentEmail = null;
+        }
+        return currentEmail;
     }
 });
