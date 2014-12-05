061b06f6cb7d74d4dd00301daa932b2b1130c4bf
Peter Pavlovich <pavlovich@gmail.com>
2014-12-04 22:59:45 -0500

    Remove meteor-UI code, reset CSS to full height for main container

--- client/main.js
+++ client/main.js
@@ -47,25 +47,6 @@ angular.module('taskMaster', ['angular-meteor'])
 
     }]);
 
-UI.registerHelper('tasks', function(){
-    if (Session.get("hideCompleted")) {
-        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-    } else {
-        return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-});
-
-Template.body.events({
-    "submit .new-taska": function (event) {
-        Meteor.call('addTask', event.target.text.value)
-        event.target.text.value = "";
-        return false;
-    },
-    "change .hide-completed input": function (event) {
-        Session.set("hideCompleted", event.target.checked);
-    }
-});
-
 Meteor.subscribe("tasks");
 
 Meteor.startup(function () {
