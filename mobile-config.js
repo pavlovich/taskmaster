bd817f2488673e13534331cb1e1eb27b98c399e0
Peter Pavlovich <pavlovich@gmail.com>
2014-12-05 02:25:29 -0500

    added mobile config file

---
+++ mobile-config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by pavlovich on 12/5/14.
+ */
+App.info({
+    id: 'com.solidgroundinnovations.taskmaster',
+    name: 'taskmaster',
+    description: 'Manage all your tasks here',
+    author: 'RWX Apps',
+    email: 'pavlovich@gmail.com',
+    website: 'http://www.pavlovich.org'
+});
